4ddaf5e5ba7bde2e85cada1b472ec88bd3fa896f
uri86 <162745737+uri86@users.noreply.github.com>
2026-02-27 10:22:30 +0200

    lib: update kprintf core to support padding

--- src/lib/print.c
+++ src/lib/print.c
@@ -71,139 +71,152 @@ void kprintf(const char *fmt, ...)
 void kvsnprintf(char *buf, size_t size, const char *fmt, va_list args)
 {
     size_t i = 0;
-
-    for (; *fmt && i < size - 1; fmt++)
+    while (*fmt && i < size - 1)
     {
+        if (*fmt != '%')
+        {
+            buf[i++] = *fmt++;
+            continue;
+        }
+
+        fmt++; // skip '%'
+
         if (*fmt == '%')
         {
-            fmt++; // skip '%'
-
-            if (*fmt == 'd') // signed decimal
-            {
-                int value = va_arg(args, int);
-                char tmp[32];
-                itoa(value, tmp, 10);
-                for (char *p = tmp; *p && i < size - 1; p++)
-                    buf[i++] = *p;
-            }
-            else if (*fmt == 'u') // unsigned decimal
-            {
-                unsigned int value = va_arg(args, unsigned int);
-                char tmp[32];
-                utoa(value, tmp, 10);
-                for (char *p = tmp; *p && i < size - 1; p++)
-                    buf[i++] = *p;
-            }
-            else if (*fmt == 's') // string
-            {
-                char *str = va_arg(args, char *);
-                if (!str)
-                    str = "(null)";
-                while (*str && i < size - 1)
-                    buf[i++] = *str++;
-            }
-            else if (*fmt == 'x' || *fmt == 'X') // hex
-            {
-                unsigned int value = va_arg(args, unsigned int);
-                char tmp[32];
-                utoa(value, tmp, 16);
-                for (char *p = tmp; *p && i < size - 1; p++)
-                    buf[i++] = *p;
-            }
-            else if (*fmt == 'b') // binary
-            {
-                unsigned int value = va_arg(args, unsigned int);
-                char tmp[64];
-                utoa(value, tmp, 2);
-                for (char *p = tmp; *p && i < size - 1; p++)
-                    buf[i++] = *p;
-            }
-            else if (*fmt == 'c')
-            {
-                unsigned int value = va_arg(args, unsigned int);
-                buf[i++] = (char)value;
-            }
-            else if (*fmt == 'l') // check for 'l' or 'll'
-            {
-                if (*(fmt + 1) == 'l') // "ll" case
-                {
-                    fmt += 2; // skip both 'l's
-                    uint64_t value = va_arg(args, uint64_t);
-                    char tmp[64];
-
-                    if (*fmt == 'u') // %llu
-                    {
-                        utoa(value, tmp, 10);
-                    }
-                    else if (*fmt == 'x' || *fmt == 'X') // %llx / %llX
-                    {
-                        utoa(value, tmp, 16);
-                        buf[i++] = '0';
-                        buf[i++] = 'x';
-                    }
-                    else if (*fmt == 'd') // %lld (signed)
-                    {
-                        itoa((int64_t)value, tmp, 10);
-                    }
-                    else
-                    {
-                        // unknown specifier, print literally
-                        buf[i++] = '%';
-                        buf[i++] = 'l';
-                        buf[i++] = 'l';
-                        buf[i++] = *fmt;
-                        continue;
-                    }
-
-                    for (char *p = tmp; *p && i < size - 1; p++)
-                        buf[i++] = *p;
-                }
-                else
-                {
-                    // single 'l'
-                    fmt++;
-                    unsigned long value = va_arg(args, unsigned long);
-                    char tmp[64];
-
-                    if (*fmt == 'u')
-                    {
-                        utoa(value, tmp, 10);
-                    }
-                    else if (*fmt == 'x' || *fmt == 'X')
-                    {
-                        utoa(value, tmp, 16);
-                        buf[i++] = '0';
-                        buf[i++] = 'x';
-                    }
-                    else if (*fmt == 'd')
-                    {
-                        itoa((long)value, tmp, 10);
-                    }
-                    else
-                    {
-                        buf[i++] = '%';
-                        buf[i++] = 'l';
-                        buf[i++] = *fmt;
-                        continue;
-                    }
-
-                    for (char *p = tmp; *p && i < size - 1; p++)
-                        buf[i++] = *p;
-                }
-            }
-
-            else // unknown specifier
-            {
-                buf[i++] = '%';
-                buf[i++] = *fmt;
-            }
+            buf[i++] = *fmt++;
+            continue;
         }
-        else
+
+        // Parse flags
+        int left_align = 0;
+        char pad_char = ' ';
+        if (*fmt == '-')
         {
-            buf[i++] = *fmt;
+            left_align = 1;
+            fmt++;
         }
-    }
+        else if (*fmt == '0')
+        {
+            pad_char = '0';
+            fmt++;
+        }
 
+        // parse width
+        int width = 0;
+        while (*fmt >= '0' && *fmt <= '9')
+        {
+            width = width * 10 + (*fmt - '0');
+            fmt++;
+        }
+
+        // parse length modifier
+        int is_long = 0, is_long_long = 0;
+        if (*fmt == 'l')
+        {
+            is_long = 1;
+            fmt++;
+            if (*fmt == 'l')
+            {
+                is_long_long = 1;
+                fmt++;
+            }
+        }
+
+        char tmp[64];
+        char *str = tmp;
+        size_t len = 0;
+
+        // parse specifier
+        switch (*fmt)
+        {
+        case 'd':
+        case 'i':
+        {
+            int64_t val = is_long_long ? va_arg(args, int64_t) : (is_long ? va_arg(args, long) : va_arg(args, int));
+            itoa((uint64_t)val, tmp, 10);
+            len = strlen(tmp);
+            break;
+        }
+        case 'u':
+        {
+            uint64_t val = is_long_long ? va_arg(args, uint64_t) : (is_long ? va_arg(args, unsigned long) : va_arg(args, unsigned int));
+            utoa(val, tmp, 10);
+            len = strlen(tmp);
+            break;
+        }
+        case 'x':
+        case 'X':
+        {
+            uint64_t val = is_long_long ? va_arg(args, uint64_t) : (is_long ? va_arg(args, unsigned long) : va_arg(args, unsigned int));
+            utoa(val, tmp, 16);
+            len = strlen(tmp);
+            break;
+        }
+        case 'p':
+        {
+            uint64_t val = (uint64_t)va_arg(args, void *);
+            tmp[0] = '0';
+            tmp[1] = 'x';
+            utoa(val, tmp + 2, 16);
+            len = strlen(tmp);
+            break;
+        }
+        case 'c':
+        {
+            tmp[0] = (char)va_arg(args, int);
+            tmp[1] = '\0';
+            len = 1;
+            break;
+        }
+        case 's':
+        {
+            str = va_arg(args, char *);
+            if (!str)
+                str = "(null)";
+            len = strlen(str);
+            break;
+        }
+        default:
+            // unsupported specifier
+            // prints the literal string
+            tmp[0] = '%';
+            tmp[1] = *fmt;
+            tmp[2] = '\0';
+            len = 2;
+            break;
+        }
+
+        int pad_len = width > (int)len ? width - (int)len : 0;
+
+        // right align (padding first)
+        if (!left_align)
+        {
+            while (pad_len > 0 && i < size - 1)
+            {
+                buf[i++] = pad_char;
+                pad_len--;
+            }
+        }
+
+        // insert string data
+        for (size_t j = 0; j < len && i < size - 1; j++)
+        {
+            buf[i++] = str[j];
+        }
+
+        // left Align
+        if (left_align)
+        {
+            while (pad_len > 0 && i < size - 1)
+            {
+                buf[i++] = ' ';
+                pad_len--;
+            }
+        }
+
+        if (*fmt)
+            fmt++;
+    }
     buf[i] = '\0';
 }
 
