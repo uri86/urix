f168b049bf493b31ba1faf2e03325dd3e63cd9f1
uri86 <uri20086@gmail.com>
2026-04-24 15:15:51 +0300

    userspace: add a buffer for commands in the shell

--- userspace/src/shell.c
+++ userspace/src/shell.c
@@ -8,13 +8,14 @@
  */
 #include "urix.h"
 #include <stdio.h>
-#include "../liburix/conf.h"
 #include <string.h>
 #include <auth.h>
+#include "../liburix/conf.h"
 
 #define MAX_ARGS 16
 #define BUF_SIZE 256
 #define PATH_SIZE 512
+#define HISTORY_MAX 20
 
 #define SHELL_CONF "/etc/shell.conf"
 #define DEFAULT_FG 7
@@ -24,87 +25,85 @@
 #define SESSION_FILE "/etc/session_user"
 
 static pid_t shell_pid;
-static uint64_t g_bg = DEFAULT_BG, g_fg = DEFAULT_FG;
+static uint64_t g_bg = DEFAULT_BG;
+static uint64_t g_fg = DEFAULT_FG;
 static uint64_t g_pc = COLOR_RED; // Path color
 static uint64_t g_rc = COLOR_RED; // Root color
+
+static char history[HISTORY_MAX][BUF_SIZE];
+static int history_count = 0;
 
 static void print_cwd_prompt(void)
 {
     char cwd[PATH_SIZE];
-    int r = getcwd(cwd, sizeof(cwd));
-    
-    char user[64];
-    strcpy(user, "user");
-    
-    // Load dynamic colors directly every prompt so config updates immediately
-    conf_t cfg;
+    char user[64] = "user";
     char val[CONF_VAL_LEN];
-    if (conf_load(&cfg, SHELL_CONF) == CONF_OK) {
-        if (conf_get(&cfg, "fg", val, sizeof(val)) == CONF_OK) g_fg = (uint64_t)atoi(val);
-        if (conf_get(&cfg, "bg", val, sizeof(val)) == CONF_OK) g_bg = (uint64_t)atoi(val);
-        if (conf_get(&cfg, "pc", val, sizeof(val)) == CONF_OK) g_pc = (uint64_t)atoi(val);
-        if (conf_get(&cfg, "rc", val, sizeof(val)) == CONF_OK) g_rc = (uint64_t)atoi(val);
+
+    int r = getcwd(cwd, sizeof(cwd));
+    conf_t cfg;
+
+    if (conf_load(&cfg, SHELL_CONF) == CONF_OK)
+    {
+        if (conf_get(&cfg, "fg", val, sizeof(val)) == CONF_OK)
+            g_fg = atoi(val);
+
+        if (conf_get(&cfg, "bg", val, sizeof(val)) == CONF_OK)
+            g_bg = atoi(val);
+
+        if (conf_get(&cfg, "pc", val, sizeof(val)) == CONF_OK)
+            g_pc = atoi(val);
+
+        if (conf_get(&cfg, "rc", val, sizeof(val)) == CONF_OK)
+            g_rc = atoi(val);
     }
-    
+
     int fd = open(SESSION_FILE, O_RDONLY);
-    if (fd >= 0) {
+    if (fd >= 0)
+    {
         ssize_t len = read(fd, user, sizeof(user) - 1);
         close(fd);
-        if (len > 0) {
+        if (len > 0)
+        {
             user[len] = '\0';
-            for (int i = 0; i < len; i++) {
-                if (user[i] == '\n' || user[i] == '\r') {
-                    user[i] = '\0';
-                    break;
-                }
-            }
+            char *p = strchr(user, '\n');
+            if (p)
+                *p = '\0';
+
+            p = strchr(user, '\r');
+            if (p)
+                *p = '\0';
         }
     }
-    
-    int is_root = (strcmp(user, "root") == 0 || strcmp(user, "admin") == 0);
-    uint64_t user_color = is_root ? g_rc : COLOR_CYAN;
 
-    change_terminal_color(user_color, g_bg);
+    int is_root = (!strcmp(user, "root") || !strcmp(user, "admin"));
+
+    change_terminal_color(is_root ? g_rc : COLOR_CYAN, g_bg);
     print(user);
+
     change_terminal_color(g_fg, g_bg);
     print("@urix:");
-    change_terminal_color(g_pc, g_bg); // apply configurable path color
-    if (r > 0)
-        print(cwd);
-    else
-        print("/");
+
+    change_terminal_color(g_pc, g_bg);
+    print(r > 0 ? cwd : "/");
     change_terminal_color(g_fg, g_bg);
-    
-    if (is_root)
-        print("# ");
-    else
-        print("> ");
+    print(is_root ? "# " : "> ");
 }
 
 static int tokenize(char *buf, char **argv, int max_args)
 {
     int argc = 0;
-    int len = strlen(buf);
-    int in_token = 0;
-
-    for (int i = 0; i <= len && argc < max_args - 1; i++)
+    while (*buf && argc < max_args - 1)
     {
-        if (buf[i] == ' ' || buf[i] == '\t' || buf[i] == '\0')
-        {
-            if (in_token)
-            {
-                buf[i] = '\0';
-                in_token = 0;
-            }
-        }
-        else
-        {
-            if (!in_token)
-            {
-                argv[argc++] = &buf[i];
-                in_token = 1;
-            }
-        }
+        while (*buf == ' ' || *buf == '\t')
+            *buf++ = '\0';
+
+        if (!*buf)
+            break;
+
+        argv[argc++] = buf;
+
+        while (*buf && *buf != ' ' && *buf != '\t')
+            buf++;
     }
     argv[argc] = NULL;
     return argc;
@@ -113,6 +112,8 @@ static int tokenize(char *buf, char **argv, int max_args)
 static int readline(char *buf, int max_len)
 {
     int i = 0;
+    int history_idx = history_count;
+
     while (i < max_len - 1)
     {
         int c = getchar();
@@ -127,12 +128,47 @@ static int readline(char *buf, int max_len)
             i--;
             print("\b \b");
         }
+        else if (c == '\x1b')
+        {
+            if (getchar() == '[')
+            {
+                int c3 = getchar();
+                if (c3 == 'A' && history_idx > 0)
+                {
+                    history_idx--;
+                }
+                else if (c3 == 'B' && history_idx < history_count)
+                {
+                    history_idx++;
+                }
+                else
+                {
+                    continue;
+                }
+
+                for (int j = 0; j < i; j++)
+                    print("\b \b");
+
+                if (history_idx < history_count)
+                {
+                    strcpy(buf, history[history_idx]);
+                    i = strlen(buf);
+                }
+                else
+                {
+                    buf[0] = '\0';
+                    i = 0;
+                }
+                print(buf);
+            }
+        }
         else if (c >= 32 && c <= 126)
         {
             buf[i++] = c;
             putchar(c);
         }
     }
+
     buf[i] = '\0';
     return i;
 }
@@ -166,12 +202,12 @@ static void load_colors(uint64_t *fg, uint64_t *bg)
         g_pc = (uint64_t)atoi(val);
     else
         conf_set(&cfg, "pc", "4");
-        
+
     if (conf_get(&cfg, "rc", val, sizeof(val)) == CONF_OK)
         g_rc = (uint64_t)atoi(val);
-    else        
+    else
         conf_set(&cfg, "rc", "4");
-    
+
     conf_save(&cfg);
 }
 
@@ -183,8 +219,6 @@ static void load_colors(uint64_t *fg, uint64_t *bg)
  */
 static void do_exec(char **argv, int argc, int fd_in, int fd_out)
 {
-    char path[PATH_SIZE];
-
     if (argc == 0 || argv[0] == NULL)
         exit(0);
 
@@ -194,33 +228,26 @@ static void do_exec(char **argv, int argc, int fd_in, int fd_out)
         dup2(fd_in, STDIN_FILENO);
         close(fd_in);
     }
+
     if (fd_out >= 0)
     {
         dup2(fd_out, STDOUT_FILENO);
         close(fd_out);
     }
 
+    char path[PATH_SIZE];
+
     if (argv[0][0] == '/' || (argv[0][0] == '.' && argv[0][1] == '/'))
     {
-        char *d = path;
-        const char *s = argv[0];
-        while (*s && d < path + PATH_SIZE - 1)
-            *d++ = *s++;
-        *d = '\0';
+        strcpy(path, argv[0]);
     }
     else
     {
-        path[0] = '/';
-        path[1] = 'b';
-        path[2] = 'i';
-        path[3] = 'n';
-        path[4] = '/';
-        path[5] = '\0';
-        char *d = path + 5;
-        const char *s = argv[0];
-        while (*s && d < path + PATH_SIZE - 1)
-            *d++ = *s++;
-        *d = '\0';
+        if (strlen(argv[0]) >= PATH_SIZE - 6)
+            exit(1);
+
+        strcpy(path, "/bin/");
+        strcat(path, argv[0]);
     }
 
     if (exec(path, argv) < 0)
@@ -238,45 +265,28 @@ static void run_parsed(char **argv, int argc, int fd_in, int fd_out)
     if (argc == 0)
         exit(0);
 
-    for (int i = 0; i < argc; i++)
+    for (int i = 0; i < argc && argv[i]; i++)
     {
-        if (argv[i] == NULL)
-            break;
+        int is_out = (strcmp(argv[i], ">") == 0);
+        int is_app = (strcmp(argv[i], ">>") == 0);
 
-        if (strcmp(argv[i], ">") == 0)
+        if (is_out || is_app)
         {
             if (argv[i + 1] != NULL)
             {
-                if (is_session_file(argv[i + 1])) {
+                if (is_session_file(argv[i + 1]))
+                {
                     println("Access denied: Session file cannot be manually edited.");
                     exit(1);
                 }
+
                 require_root_for_ps(argv[i + 1]);
-                int rfd = open(argv[i + 1], O_WRONLY | O_CREAT | O_TRUNC);
-                if (rfd >= 0)
+                int fd = open(argv[i + 1], O_WRONLY | O_CREAT | (is_app ? O_APPEND : O_TRUNC));
+
+                if (fd >= 0)
                 {
-                    dup2(rfd, STDOUT_FILENO);
-                    close(rfd);
-                }
-            }
-            argv[i] = NULL;
-            argc = i;
-            break;
-        }
-        else if (strcmp(argv[i], ">>") == 0)
-        {
-            if (argv[i + 1] != NULL)
-            {
-                if (is_session_file(argv[i + 1])) {
-                    println("Access denied: Session file cannot be manually edited.");
-                    exit(1);
-                }
-                require_root_for_ps(argv[i + 1]);
-                int rfd = open(argv[i + 1], O_WRONLY | O_CREAT | O_APPEND);
-                if (rfd >= 0)
-                {
-                    dup2(rfd, STDOUT_FILENO);
-                    close(rfd);
+                    dup2(fd, STDOUT_FILENO);
+                    close(fd);
                 }
             }
             argv[i] = NULL;
@@ -285,14 +295,23 @@ static void run_parsed(char **argv, int argc, int fd_in, int fd_out)
         }
     }
 
-    if (argc == 0 || argv[0] == NULL) exit(0);
+    if (argc == 0 || argv[0] == NULL)
+        exit(0);
 
-    /* Process pid built-in */
     if (strcmp(argv[0], "pid") == 0)
     {
-        if (fd_in >= 0) { dup2(fd_in, STDIN_FILENO); close(fd_in); }
-        if (fd_out >= 0) { dup2(fd_out, STDOUT_FILENO); close(fd_out); }
-        
+        if (fd_in >= 0)
+        {
+            dup2(fd_in, STDIN_FILENO);
+            close(fd_in);
+        }
+
+        if (fd_out >= 0)
+        {
+            dup2(fd_out, STDOUT_FILENO);
+            close(fd_out);
+        }
+
         char out[32];
         snprintf(out, sizeof(out), "PID: %d", shell_pid);
         println(out);
@@ -312,10 +331,7 @@ static void run_parsed(char **argv, int argc, int fd_in, int fd_out)
  */
 static void run_cmd(char *cmd_buf, int fd_in, int fd_out)
 {
-    char *argv[MAX_ARGS];
-    for (int i = 0; i < MAX_ARGS; i++)
-        argv[i] = NULL;
-
+    char *argv[MAX_ARGS] = {NULL};
     int argc = tokenize(cmd_buf, argv, MAX_ARGS);
     run_parsed(argv, argc, fd_in, fd_out);
 }
@@ -326,6 +342,7 @@ static void run_cmd(char *cmd_buf, int fd_in, int fd_out)
 static void run_piped(char *left_buf, char *right_buf)
 {
     int pipefd[2];
+
     if (pipe(pipefd) < 0)
     {
         println("pipe: failed to create pipe");
@@ -355,14 +372,13 @@ static void run_piped(char *left_buf, char *right_buf)
 int main(void)
 {
     shell_pid = getpid();
-    
     clear_screen();
     prntlg();
-    
+
     load_colors(&g_fg, &g_bg);
     change_terminal_color(g_fg, g_bg);
 
-    println("URIX Shell v0.2");
+    println("URIX Shell v0.3");
     println("Type 'help' for commands");
 
     char buf[BUF_SIZE];
@@ -373,56 +389,62 @@ int main(void)
         print("\x1b[?25h");
         print_cwd_prompt();
 
-        int len = readline(buf, sizeof(buf));
-        if (len == 0)
+        if (readline(buf, sizeof(buf)) == 0)
             continue;
-            
+
         strcpy(buf_copy, buf);
 
-        char *pipe_pos = NULL;
-        for (int i = 0; buf[i] != '\0'; i++)
+        if (history_count == 0 || strcmp(history[history_count - 1], buf_copy) != 0)
         {
-            if (buf[i] == '|')
+            if (history_count < HISTORY_MAX)
             {
-                pipe_pos = &buf[i];
-                break;
+                strcpy(history[history_count++], buf_copy);
+            }
+            else
+            {
+                for (int j = 1; j < HISTORY_MAX; j++)
+                {
+                    strcpy(history[j - 1], history[j]);
+                }
+                strcpy(history[HISTORY_MAX - 1], buf_copy);
             }
         }
 
-        if (pipe_pos != NULL)
+        char *pipe_pos = strchr(buf, '|');
+
+        if (pipe_pos)
         {
-            *pipe_pos = '\0';
-            char *left_cmd = buf;
-            char *right_cmd = pipe_pos + 1;
-            while (*right_cmd == ' ' || *right_cmd == '\t')
-                right_cmd++;
-            run_piped(left_cmd, right_cmd);
+            *pipe_pos++ = '\0';
+            while (*pipe_pos == ' ' || *pipe_pos == '\t')
+                pipe_pos++;
+
+            run_piped(buf, pipe_pos);
             continue;
         }
 
-        char *argv[MAX_ARGS];
-        for (int i = 0; i < MAX_ARGS; i++) argv[i] = NULL;
+        char *argv[MAX_ARGS] = {NULL};
         int argc = tokenize(buf_copy, argv, MAX_ARGS);
-        
-        if (argc == 0) continue;
-        
-        // Parent-side builtins that must mutate the shell itself
+
+        if (argc == 0)
+            continue;
+
         if (strcmp(argv[0], "exit") == 0)
         {
             println("Goodbye!");
             exit(0);
         }
-        
+
         if (strcmp(argv[0], "cd") == 0)
         {
-            const char *target = "/";
-            if (argc > 1) {
-                target = argv[1];
+            const char *dst = "/";
+            if (argc > 1)
+            {
+                dst = argv[1];
             }
-            if (chdir(target) < 0)
+            if (chdir(dst) < 0)
             {
                 print("cd: no such directory: ");
-                println(target);
+                println(dst);
             }
             continue;
         }
